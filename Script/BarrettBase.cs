e8582373631ca315406bf99d60a3514345729a9e
oic-kinno-takanobu <kinno1816@gmail.com>
2018-06-30 16:59:21 +0900

    BarrettBase.cs作成

---
+++ Script/BarrettBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrettBase : MonoBehaviour {
+
+    [SerializeField]
+    protected float barrettSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected void OnCollisionEnter(Collision collision) {
+        
+    }
+}
