f685b1afff4dfdd53548c9ed24340d2957af8151
oic-kinno-takanobu <kinno1816@gmail.com>
2018-06-30 13:44:36 +0900

    ベース作成

---
+++ Script/CharBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharBase : MonoBehaviour {
+
+    [SerializeField]
+    protected GameObject[] shotObjList;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// 弾を取得する処理
+    /// </summary>
+    /// <param name="indx"></param>
+    /// <returns></returns>
+    protected GameObject GetShotObj(int indx) {
+        return shotObjList[indx];
+    }
+}
